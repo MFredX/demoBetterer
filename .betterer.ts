12472a11321d3b7f577c24b557ac06e558370090
Sachin Fernando <sachin.fernando@orica.com>
2022-12-01 09:13:45 +1100

    add new betterer test, add new errors, update results file

--- .betterer.ts
+++ .betterer.ts
@@ -4,5 +4,9 @@ export default {
   // Add tests here ☀️
   '[typescript] strictNullChecks': () => typescript(`./tsconfig.json`, {
     strictNullChecks: true
-  }).include('./src/**/*.ts')
+  }).include('./src/**/*.ts'),
+  
+  '[typescript] strictChecks': () => typescript(`./tsconfig.json`, {
+    strict: true
+  }).include('./src/**/*.ts'),
 };
